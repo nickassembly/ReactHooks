02e965ea68fb7a21e8d1ebc698c21d4bd06584ac
nickguerra <nickguerra@gmail.com>
2021-04-12 18:50:04 -0500

    created firebase rt database for holding ingredients

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,10 +8,18 @@ const Ingredients = () => {
   const [ userIngredients, setUserIngredients ] = useState([]);
 
   const addIngredientHandler = ingredient => {
-setUserIngredients(prevIngredients => [
-  ...prevIngredients,
-   {id: Math.random().toString(), ...ingredient }
-  ]);
+fetch('https://react-hooks-update-5bcb2-default-rtdb.firebaseio.com/ingredients.json', {
+  method: 'POST',
+  body: JSON.stringify(ingredient),
+  headers: { 'Content-Type': 'application/json' }
+}).then(response => {
+  return response.json(); 
+}).then(responseData => {
+  setUserIngredients(prevIngredients => [
+    ...prevIngredients,
+     {id: responseData.name, ...ingredient }
+    ]);
+});
   };
 
   return (
