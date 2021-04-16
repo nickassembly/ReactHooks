33d62b86cfb28a5507b6a078d2139ab1f85a2561
nickguerra <nickguerra@gmail.com>
2021-04-16 06:35:13 -0500

    added check for timeout before sending request on search component

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,32 +6,40 @@ import './Search.css';
 const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState('');
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query = enteredFilter.length === 0 
-    ? '' 
-    : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch('https://react-hooks-update-5bcb2-default-rtdb.firebaseio.com/ingredients.json' + query)
-    .then(response => response.json())
-    .then(responseData => {
-  const loadedIngredients = [];
-  for (const key in responseData) {
-    loadedIngredients.push({
-      id: key,
-      title: responseData[key].title,
-      amount: responseData[key].amount
-      });
-     }
-    // onLoadIngredients(loadedIngredients);
-    });
-  }, [enteredFilter, onLoadIngredients]);
+    setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query = enteredFilter.length === 0 
+        ? '' 
+        : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch('https://react-hooks-update-5bcb2-default-rtdb.firebaseio.com/ingredients.json' + query)
+        .then(response => response.json())
+        .then(responseData => {
+      const loadedIngredients = [];
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+          });
+         }
+         onLoadIngredients(loadedIngredients);
+        });
+      }
+      }, 500);
+      }, [enteredFilter, onLoadIngredients, inputRef]);  
 
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={enteredFilter} onChange={event => setEnteredFilter(event.target.value)}/>
+          <input
+          ref={inputRef}
+           type="text"
+            value={enteredFilter} onChange={event => setEnteredFilter(event.target.value)}/>
         </div>
       </Card>
     </section>
