e41f736f468663878a253e0fbfba19e72e1b1d40
nickguerra <nickguerra@gmail.com>
2021-04-20 19:26:16 -0500

    added use reducer

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,12 +1,27 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useReducer, useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case 'SET': 
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients, action.ingredient]
+    case 'DELETE': 
+      return currentIngredients.filter(ing => ing.id !== action.id);
+      default: 
+      throw new Error('Should not get there!');
+  }
+};
+
+
 const Ingredients = () => {
-  const [ userIngredients, setUserIngredients ] = useState([]);
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  //const [ userIngredients, setUserIngredients ] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
 
@@ -15,7 +30,8 @@ console.log('RENDERING INGREDIENTS', userIngredients);
   }, [userIngredients]);
 
 const filteredIngredientsHandler = useCallback(filteredIngredients => {
-setUserIngredients(filteredIngredients); 
+// setUserIngredients(filteredIngredients); 
+dispatch({type: 'SET', ingredients: filteredIngredients});
 }, []);
 
   const addIngredientHandler = ingredient => {
@@ -28,10 +44,11 @@ fetch('https://react-hooks-update-5bcb2-default-rtdb.firebaseio.com/ingredients.
   setIsLoading(false);
   return response.json(); 
 }).then(responseData => {
-  setUserIngredients(prevIngredients => [
-    ...prevIngredients,
-     {id: responseData.name, ...ingredient }
-    ]);
+  // setUserIngredients(prevIngredients => [
+  //   ...prevIngredients,
+  //    {id: responseData.name, ...ingredient }
+  //   ]);
+  dispatch({type: 'ADD', ingredient: { id: responseData.name, ...ingredient }});
 });
   };
 
@@ -41,9 +58,10 @@ fetch('https://react-hooks-update-5bcb2-default-rtdb.firebaseio.com/ingredients.
   method: 'DELETE'
 }).then(response => {
   setIsLoading(false);
-  setUserIngredients(prevIngredients => 
-    prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
-    );
+  // setUserIngredients(prevIngredients => 
+  //   prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+  //   );
+  dispatch({type: 'DELETE', id: ingredientId});
 }).catch(error => {
 setError('Something went wrong!');
 setIsLoading(false);
