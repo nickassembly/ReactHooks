dbe007c52ba389bf6c761968abc167d8f93c8923
nickguerra <nickg@maliciousgames.com>
2023-07-16 06:22:46 -0500

    added use effect hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import Auth from './components/Auth';
 import { AuthContext } from './context/auth-context';
 import Ingredients from './components/Ingredients/Ingredients';
@@ -18,13 +18,21 @@ import Ingredients from './components/Ingredients/Ingredients';
 // export default App;
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    document.title = input;
+    console.log('useEffect');
+  }, [input]);
+
 
   return (
     <>
-      <h1> {count}</h1>
-
-      <button onClick={() => setCount(count + 1)}>Increment Count</button>
+      <h1>useState - {counter}</h1>
+      <h2>{input}</h2>
+      <h1 onClick={() => setCounter((prevCounter) => prevCounter + 1)}>Use Effect </h1>
+      <input onChange={(e) => setInput(e.target.value)} type="text" />
     </>
   )
 
